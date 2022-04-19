99ab00612b5a4eba82e6c68252e4e156d829d975
mmmmasum <mmm.masum@yahoo.com>
2022-04-19 22:34:59 +0600

    about section done

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h1>About page</h1>
+        <div className="w-50 mx-auto mt-4" text-center>
+            <h1 className="text-center">About Me</h1>
+            <h3 className="mt-4">Md Mahmuduzzaman Masum</h3>
+            <p>I am a gym trainer. I have training peoples for several years. Join my class as soon as possible. I am giving you the guaranty that you will surely get benefitted. If not, you will get your money back. My goal is to give people a proper guideline so that they won't follow wrong steps while doing exercise. </p>
         </div>
     );
 };
